7c14e0d05ef6e897d087676389f8a2b06bccc225
Clayton <caturner3@g.cofc.edu>
2017-03-17 15:11:01 -0500

    Removed excess code. Added comments & gitignore. Full example works/added.

--- power_hour.py
+++ power_hour.py
@@ -1,18 +1,10 @@
-# Adapted from: https://github.com/michaelpippolito/power_hour/blob/master/powerhour.py
-
 import os
 import subprocess
-import ffmpeg_split
-
-def timeStampConvert(timeStamp):
-    minutes = int(timeStamp[0:timeStamp.rfind(":")])
-    seconds = int(timeStamp[timeStamp.rfind(":") + 1:])
-    return (minutes * 60000) + (seconds * 1000)
 
 
-def timeStampConvertToSeconds(timeStamp):
-    minutes = int(timeStamp[0:timeStamp.rfind(":")])
-    seconds = int(timeStamp[timeStamp.rfind(":") + 1:])
+def timestamp_convert_to_seconds(timestamp):
+    minutes = int(timestamp[0:timestamp.rfind(":")])
+    seconds = int(timestamp[timestamp.rfind(":") + 1:])
     return ((minutes * 60000) + (seconds * 1000)) / 1000
 
 
@@ -25,81 +17,64 @@ subprocess.call("youtube-dl --quiet -f mp4 -o beep_full.mp4 "
                 + song_file.readline(), shell=True)
 
 # Cut beep video
-beepStart = timeStampConvertToSeconds(song_file.readline())
-beepEnd = timeStampConvertToSeconds(song_file.readline())
+beepStart = timestamp_convert_to_seconds(song_file.readline())
+beepEnd = timestamp_convert_to_seconds(song_file.readline())
 beepLength = beepEnd - beepStart
-filename = "beep.csv"
-with open(filename, "w+") as csvfile:
-    csvfile.write("start_time,length,rename_to\n")
-    csvfile.write(str(beepStart) + "," + str(beepLength) + ",beep.mp4")
-#ffmpeg_split.split_by_manifest("beep_full.mp4", filename)#, vcodec="h264")
+
+# Convert beep to transport stream for intersplicing later
 subprocess.call("ffmpeg -ss " + str(beepStart) + " -i beep_full.mp4" +
                         " -t " + str(beepLength) + " -vcodec libx264 -acodec aac -strict experimental -r 24 -async 1 -y beep.mp4")
-#subprocess.call("ffmpeg -i beep.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts beep.ts"
 subprocess.call("ffmpeg -i beep.mp4 -vf scale=1280:720,setdar=16:9 beep.ts"
                 , shell=True)
+
+# Now that we have the transport stream, let's remove the spliced and original beep
 os.remove("beep_full.mp4")
-#os.remove("beep.mp4")
-os.remove(filename)
+os.remove("beep.mp4")
 
-# beep = beep[beepStart:beepEnd]
-# save beep
-# remove full beep video and rename to what we want
-
+# TODO: Create a GUI to manage this as people may want differing song amounts/lengths/other customization
+# Default: 60 songs of 1 minute a piece
+# concat_vid_string holds the final string of transport streams to be passed to ffmpeg's concat protocol tool
 songLength = 60
 duration = "01:00"
 concat_vid_string = "concat:"
-concat_vid_string_mp4 = ""
-concat_vid_filter = "ffmpeg "
-for i in range(1, 61):
+for i in range(1, songLength + 1):
     link = song_file.readline()
 
+    # This if is to prevent errors happening from extra blank lines
     if link:
         curSong = str(i)
 
+        # Read the start time and convert
         start_time = song_file.readline().strip()
-        start = timeStampConvertToSeconds(start_time)
+        start = timestamp_convert_to_seconds(start_time)
 
+        # Download the full video that we want to split up and create transport stream just like above
+        # TODO: We could extract this process into a function cleanly, but it's not bad as is
         print("Downloading song " + curSong + " of 60...")
         subprocess.call("youtube-dl --quiet -f mp4 -o " + curSong + "_full.mp4 " + link, shell=True)
-        #filename = curSong + ".csv"
-        #with open(filename, "w+") as csvfile:
-        #    csvfile.write("start_time,length,rename_to\n")
-        #    csvfile.write(str(start) + "," + str(songLength) + "," + curSong + ".mp4")
-        #ffmpeg_split.split_by_manifest(curSong + "_full.mp4", filename)#, vcodec="h264")
         subprocess.call("ffmpeg " +
                         " -ss " + str(start_time) + " -i " + curSong + "_full.mp4 " +
                         " -t " + duration + " -vcodec libx264 -acodec aac -strict experimental -r 24 -async 1 -y " + curSong + ".mp4")
         os.remove(curSong + "_full.mp4")
-        #os.remove(curSong+".csv")
 
-        #subprocess.call("ffmpeg -i " + curSong + ".mp4 -c copy  " + curSong + ".ts"
-        subprocess.call("ffmpeg -i " + curSong +".mp4 -vf scale=1280:720,setdar=16:9 " + curSong + ".ts"
+        subprocess.call("ffmpeg -i " + curSong + ".mp4 -vf scale=1280:720,setdar=16:9 " + curSong + ".ts"
                         , shell=True)
-        #os.remove(curSong+".mp4")
+        os.remove(curSong+".mp4")
+
+        # Add our transport stream to the list and follow it with the interspliced beep
         concat_vid_string += curSong + ".ts|beep.ts|"
-        concat_vid_string_mp4 += "file " + curSong + ".mp4\n"
-        concat_vid_filter += "-i " + curSong + ".mp4 "
 
-# Protocol attempt
-print(concat_vid_string,concat_vid_string[:-9])
-#subprocess.call("ffmpeg -i \"" + concat_vid_string[:-9] + "\"  -c copy -bsf:a aac_adtstoasc powerhour.mp4")
+
+# Close the song file since we are done reading it
+song_file.close()
+
+# Use the ffmpeg Protocol method
+# [:-9] is so we remove that last beep off the end
+# TODO: When GUI'd up, do custom naming/location saving
 subprocess.call("ffmpeg -i \"" + concat_vid_string[:-9] + "\"  -c copy -s 1280:720 powerhour.mp4")
-#subprocess.call("ffmpeg -i \"" + concat_vid_string[:-9] + "\" -vf scale=1280:-1:force_original_aspect_ratio=decrease powerhour.mp4", shell = True)
 
-# Demuxer attempt
-##f = open("inputs.txt","w")
-##f.write(concat_vid_string_mp4[:-1])
-##f.close()
-##subprocess.call("ffmpeg -f concat -i inputs.txt -vcodec copy -acodec copy Power_Hour.mp4"
-##                , shell=True)
-#subprocess.call("ffmpeg -i 1.mp4 -i 2.mp4 -i 3.mp4 -vcodec copy -acodec copy Power_Hour.mp4")
 
-# Filter attempt
-#concat_vid_filter += "-vf scale=1280:720:force_original_aspect_ratio=decrease powerhour.mp4"
-#subprocess.call(concat_vid_filter, shell=True)
-
-# When debugging, comment this out
+# Remove temp files TODO: put in a temp folder and just remove the folder and reference through it
 for root, dirs, files in os.walk(os.getcwd()):
     for currentFile in files:
         if any(currentFile.lower().endswith(ext) for ext in ["ts"]):
